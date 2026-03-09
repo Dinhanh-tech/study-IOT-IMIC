170365530c65ebb48cb17c3bdb549e6c72f07a70
Dinhanh-tech <luongdinhanh0357421041@gmail.com>
2026-03-10 00:29:11 +0700

    blink code

---
+++ Dinhanh/BLINK.ino
@@ -0,0 +1,15 @@
+#define GPIO2 2
+void setup() {
+  pinMode(GPIO2,OUTPUT);
+
+}
+
+void loop() {
+  // thiet lap chan 2 co dong dien
+digitalWrite(GPIO2,LOW);
+//delay khoang 1s
+delay(1000);
+//thet lap chan 2 khong co dong dien
+digitalWrite(GPIO2,HIGH);
+delay(2000);
+}
